c24bf215ba1bc38eeda3e8631c47e2fdd1f146d8
LuisRemote <ortizangeles.mx@gmail.com>
2022-07-19 17:15:57 -0500

    OOP principles added

--- item.py
+++ item.py
@@ -4,20 +4,27 @@ class Item:
     pay_rate = 0.8 #
     all = []
     def __init__(self, name: str, price: float, quantity=0):
-        """
-        Constructor 
-        """
         # Run validations to the received arguments
         assert price >= 0, f"Price {price} is not greater or equal to zero!"
         assert quantity >= 0, f"Quantity {quantity} is not greater or equal to zero!"
 
         # Assign to self object
         self.__name = name
-        self.price = price
+        self.__price = price
         self.quantity = quantity
 
         # Actions to execute
         Item.all.append(self)
+
+    @property
+    def price(self):
+        return self.__price
+
+    def apply_discount(self):
+        self.__price = self.__price * self.pay_rate
+
+    def apply_increment(self, increment_value):
+        self.__price = self.__price = self.__price + self.__price * increment_value
 
     @property
     # Property Decorator = Read-Only Attribute
@@ -32,10 +39,8 @@ class Item:
             self.__name = value
 
     def calculate_total_price(self):
-        return self.price * self.quantity
+        return self.__price * self.quantity
 
-    def apply_discount(self):
-        self.price = self.price * self.pay_rate
 
     @classmethod
     def instantiate_from_csv(cls):
@@ -65,3 +70,20 @@ class Item:
     def __repr__(self):
         return f"{self.__class__.__name__}('{self.name}', {self.price}, {self.quantity})"
 
+    def __connect(self, smpt_server):
+        pass
+
+    def __prepare_body(self):
+        return f"""
+        Hello Someone.
+        We have {self.name} {self.quantity} times.
+        Regars, Remote Worker
+        """
+
+    def __send(self):
+        pass
+
+    def send_email(self):
+        self.__connect('')
+        self.__prepare_body()
+        self.__send()
--- main.py
+++ main.py
@@ -1,9 +1,33 @@
 from item import Item
+from phone import Phone
+from keyboard import Keyboard
 
-item1 = Item("MyItem", 750)
 
-# Setting an Attribute
-item1.name = "OtherItem"
+# Encapsulation
 
+# item1 = Item("MyItem", 750)
+# item1.apply_increment(0.2)
+# item1.apply_discount()
 # Getting an Attibute
-print(item1.name)
+# print(item1.price)
+
+
+# Abstraction
+
+# item1 = Item("MyItem", 750, 6)
+# item1.send_email()
+
+
+# Inheritance
+
+# item1 = Phone("rwPhone", 1000, 3)
+# item1.apply_increment(0.2)
+# print(item1.price)
+
+
+# Polymorphism
+
+item1 = Keyboard("rwKeyboard", 1000, 3)
+item1.apply_discount()
+
+print(item1.price)
